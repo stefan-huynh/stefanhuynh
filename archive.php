e90974d8c7e3bbf60276cb6e3f8925e069108146
Stefan <stefan@Stefans-MacBook-Pro.local>
2018-04-23 21:16:51 +0200

    initial commit

---
+++ archive.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package blank
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
